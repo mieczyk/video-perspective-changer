dffd2afaf69dcedaa71ef60c847f7f5a7007f6f0
mieczyk <mieczkowski.lukasz@gmail.com>
2019-04-24 12:29:42 +0200

    Modify selection by dragging vertices

--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ class Window:
     def __init__(self, name):
         self._name = name
         self._selecting = False
+        self._dragging = False
 
         self.selection = None
 
@@ -15,13 +16,24 @@ class Window:
     
     def _mouse_callback(self, event, x, y, flags, userdata):
         if event == cv2.EVENT_LBUTTONDOWN:
-            self._selecting = True
-            self.selection = Selection([x,y])
-        elif event == cv2.EVENT_MOUSEMOVE and self._selecting:
-            self.selection.update([x,y])
+            if self.selection is not None and self.selection.activate_vertex([x,y]):
+                self._dragging = True
+            else:
+                self._selecting = True
+                self.selection = Selection([x,y])
+        elif event == cv2.EVENT_MOUSEMOVE: 
+            if self._selecting:
+                self.selection.update([x,y])
+            elif self._dragging:
+                self.selection.update_active_vertex([x,y])
         elif event == cv2.EVENT_LBUTTONUP:
-            self.selection.update([x,y])
-            self._selecting = False
+            if self._selecting:
+                self.selection.update([x,y])
+                self._selecting = False
+            elif self._dragging:
+                self.selection.update_active_vertex([x,y])
+                self.selection.deactivate_vertex()
+                self._dragging = False
 
     def dispose(self):
         cv2.destroyWindow(self._name)
@@ -37,6 +49,8 @@ class Window:
 
 class Selection:
     def __init__(self, start_point):
+        self._active_vertex_idx = -1
+
         self.vertices = np.full((4,2), start_point, dtype=int)
     
     def update(self, end_point):
@@ -55,6 +69,29 @@ class Selection:
         else:
             self.vertices[1] = [start_point[0], end_point[1]]
             self.vertices[3] = [end_point[0], start_point[1]] 
+    
+    def update_active_vertex(self, end_point):
+        if self._active_vertex_idx > -1:
+            self.vertices[self._active_vertex_idx] = end_point
+
+    def activate_vertex(self, point, margin=20):
+        matched_point_enum = (
+            idx for idx, vertex 
+            in enumerate(self.vertices) 
+            if self._points_match(vertex, point, margin)
+        ) 
+        self._active_vertex_idx = next(matched_point_enum, -1)
+
+        return self._active_vertex_idx > -1
+
+    def deactivate_vertex(self):
+        self._active_vertex_idx = -1
+
+    def _points_match(self, p1, p2, margin):
+        return (
+            p2[0]-margin <= p1[0] <= p2[0]+margin 
+            and p2[1]-margin <= p1[1] <= p2[1]+margin
+        )
 
 def wait_for_key(miliseconds):
     # '& 0xFF' because we're interested in the last 8 bits only.
